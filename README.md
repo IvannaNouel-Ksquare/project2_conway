67ce3c7dabf04687feb6303510c4f446d5d26bc6
Ivanna Nouel <Ivanna.Nouel@itksquare.edu.mx>
2022-10-12 08:45:05 -0500

    Homework

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
-# Project I
+# Project II - The Game of Life
 
-Welcome! This repo contains helpful information and starter files for your project, just fork this repo and go to town!
+The Game of Life is a cellular automaton devised by the British mathematician John Horton Conway. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves.
+
+The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, populated and unpopulated. Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.
+
+At each step in time, the following transitions occur:
+
+1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
 ## Team members
 
-Write here the names, career path and email of the members of the team
+| Name | Career path | Email |
+| :---:         |     :---:      |          :---: |
+| Diego Velazquez| Salesforce | diego.velazquez@itksquare.edu.mx |
+| Ivanna Nouel | JavaScript | ivanna.nouel@itksquare.edu.mx |
+| Tomas García | Flutter | tomas.garcia@itksquare.edu.mx |
 
 ## Assignation 
 
 Please use this format for your User Stories assignations
 
-| User Stories     | John Doe | Jane Doe |
-| ---------------- | :--: | ---: |
-| Add US text here |  X   |      |
-| Add US text here |      |    X |
-| Add US text here |  X   |      |
+| User Stories     | Diego Velazquez | Ivanna Nouel | Tomas García |
+| ---------------- | :--: | :--: | :--: |
+|0- Game Loop |   x   |   x   |   x   |
+|1- As a user, whenever I navigate to the website and see an empty grid with the option to let the user set up the initial configuration. |      |      |   x   |
+|2- If the game is paused I can resume it |      |   x   |      |
+|3- I can clear the board |      |   x   |      |
+|4- I can click a 'Randomize' button that will generate an initial configuration for the game to play and this can only be done when the games has not started or is paused.|      |      |   x   |
+|5- I can see the amount of generation has passed since the start of the game |   x   |      |      |
+|6- I can click a button that would set the game back to the initial state (how the website initially loads in) |   x   |      |      |
+|7- (Optional) Research know patterns that occurs in the Game Of Life and allow the  user to see those patterns |   x   |   x   |   x   |
 
 # Live demo
 
-Please include here a link to your [live demo](url_here_please)
-
-# Requeriments
-You need to use the same version of node that we use in class and no external dependices can be used. This means no frameworks or libraries.
-
+https://ivannanouel-ksquare.github.io/project2_conway/
---
+++ gameLoop.js
@@ -0,0 +1,353 @@
+let btnStart = document.querySelector(".start");
+let btnClear = document.querySelector(".clear");
+let btnRandom = document.querySelector(".randomize");
+let btnRestart= document.querySelector(".restart");
+let genCounter = document.querySelector(".gen");
+let table = document.querySelector('#table');
+let menuPat1 = document.querySelector('#pat1');
+let menuPat2 = document.querySelector('#pat2');
+let menuPat3 = document.querySelector("#pat3");
+let menuPat4 = document.querySelector("#pat4");
+let menu = document.querySelector('.patterns');
+
+let xySize = 20; // Height and width of the grid
+let htmlArr = []; // HTML grid
+let cells = []; // Logical grid
+let playing = false;// Flag to check if the game is being played 
+let timer; //timer to execute at a particular time
+let genNum=0;
+
+
+
+//impure function
+function createTable() {
+
+    for (let y = 0; y < xySize; y++) { // Height
+
+        // Generates the <tr></tr> wich there will be the Y coordinate in the HTML grid
+        let tr = document.createElement('tr'); // Creates <tr></tr>
+        let tdArr = []; // Saves the <td> elements
+
+        cells.push(new Array(xySize).fill(0)); // Creates an array and assigns values to it, the "fill()" method fills specified elements in an array with a value
+        htmlArr.push(tdArr); // the "push()" adds new items to the end of an array, in this case the "tdArr"
+        table.appendChild(tr); // the "appendChild()" appends a element as the last child of an element, in this case "tr" variable
+
+        for (let x = 0; x < xySize; x++) { // Width
+
+            // Generates the <td></td> with an "id" and "onmouseup" function, then append it in the respective <tr></tr>
+            let td = document.createElement('td'); // Creates <td></td>
+            td.setAttribute("id", `coor-${x + "-" + y}`); // Adds an id to the <td> which will be a position with respect to a coordinate
+            td.setAttribute("onmouseup", `search(${x},${y})`); // Adds an onmouseup function to the <td>
+            tdArr.push(td); // Saves the <td> into the "tdArr" array
+            tr.appendChild(td); // The appendChild() method appends a element as the last child of an element
+        }
+    }
+
+}
+
+//impure function
+
+
+function draw() {
+    for (let y = 0; y < xySize; y++) { // Height
+
+        for (let x = 0; x < xySize; x++) { // Width
+
+            // The "setAttribute()" method sets a new value to an attribute
+            // Sets to the grid cell a color adding a class to the <td> according to the "cells[y][x]" value
+            // 1 = Filled = black
+            // 0 = Empty = white
+            htmlArr[y][x].setAttribute('class', 'cell ' + (cells[y][x] === 1 ? 'filled' : 'empty'));
+        }
+    }
+}
+
+
+//impure function
+
+function newGeneration() {
+    
+    
+    let newCells = []; // Save the new logical grid
+
+    for (let y = 0; y < xySize; y++) { // Temporal logical grid
+
+        newCells.push(new Array(xySize).fill(0));  // Creates an array and assigns values to it, the "fill()" method fills specified elements in an array with a value
+
+    }
+
+    for (let y = 0; y < xySize; y++) { // Height
+
+        for (let x = 0; x < xySize; x++) { // Width
+
+            let neibhours = neighboursCount(x, y); // Search for neighbours arround the cell
+
+            if (cells[y][x] === 0 && neibhours === 3) { // if the cell is empty and has 3 neighbours
+                newCells[y][x] = 1; // Fills the cell
+            }
+
+            if (cells[y][x] === 1 && (neibhours === 2 || neibhours === 3)) { // if the cell is filled and has 3 or 2 neighbours
+                newCells[y][x] = 1; // The cell stays
+            }
+        }
+    }
+    cells = newCells; // Updates the logical grid
+    genNum++;//increases the number of generations
+    genCounter.innerHTML= "Generation: " + genNum; //refresh the number of generatios each iteration
+    draw(); // Updates the visual grid
+}
+
+
+//impure function
+
+function neighboursCount(x, y) {
+    let count = 0;
+
+    // Get over though the total neighbourhood next to the central cell (0,0)
+    for (h = -1; h <= 1; h++) { // Y -1 to Y + 1
+
+        for (w = -1; w <= 1; w++) { // X - 1 to X + 1
+
+            let nx = (x + w + xySize) % xySize;
+            let ny = (y + h + xySize) % xySize;
+
+            count = count + cells[ny][nx];
+        }
+    }
+
+    return count - cells[y][x]; // return the total neighbours minus the input cell
+}
+
+
+//impure function
+
+function init() { // Initialize the game
+    createTable();
+    draw();
+    btnRestart.classList.add("clickDisable"); 
+    btnRestart.onclick = () =>reset();
+    btnStart.onclick = () => start();
+    btnClear.onclick = () => clear();
+    btnRandom.onclick = () => randomize();
+    menuPat1.onclick = () => loadpat(pattern1);
+    menuPat2.onclick = () => loadpat(pattern2);
+    menuPat3.onclick = () => loadpat(pattern3);
+    menuPat4.onclick = () => loadpat(pattern4);
+    menu.classList.add("patternst");
+}
+
+
+//impure function
+
+function search(x, y) { // Search for the <td> with the "id" (id = coordinate)
+
+    // Black to white
+    if (document.querySelector(`#coor-${x + "-" + y}`).classList[1] === "filled") { //Check if the <td> has the "filled" class
+        document.querySelector(`#coor-${x + "-" + y}`).classList.remove("filled"); // Remove the "filled" class in the <td>
+        document.querySelector(`#coor-${x + "-" + y}`).classList.add("empty"); // Add the "empty" class in the <td>
+        cells[y][x] = 0; // Change the cell value
+    }
+
+    // White to black
+    else {
+        document.querySelector(`#coor-${x + "-" + y}`).classList.remove("empty"); //Remove the "empty" class in the <td>
+        document.querySelector(`#coor-${x + "-" + y}`).classList.add("filled"); // Add the "filled" class in the <td>
+        cells[y][x] = 1;
+    }
+}
+
+//impure function
+
+
+//This function is when the button start is clicked - starts and pause the game
+function start() {
+    btnRestart.classList.remove("clickDisable"); //enables the reset button
+    if (playing) {
+        playing = false; //if playing equals false the timer will clear and the games stops
+        clearTimeout(timer);//sets the timer to 0
+
+        btnStart.innerHTML = 'Resume'; //displays Resume on the button
+
+    } else {
+        menu.classList.remove("patternst");
+        table.classList.add("pointerDiable");
+        genCounter.classList.add("avaible");
+        btnRandom.classList.add("clickDisable"); // Adds "clickDisable" class into the "btnRandom" to make it unclickeable and hide it
+        menu.classList.add("clikDisable");
+        //starts the game 
+        btnStart.innerHTML = 'Pause';//displays pause on the button
+        
+        playing = true;
+        play();
+    }
+
+}
+
+//impure function
+
+
+//This function is to run the game 
+function play() {
+
+    //This is used to call the function to generate the generation 
+    newGeneration();
+
+    if (playing) {//if playing equals true the game starts playing with an interval of 100
+        timer = setTimeout(play, 100); //will call the function play that run the game and execute the new generation
+    }
+}
+
+
+
+//impure function
+
+//this function is to clear the board
+function clear() {
+    //we create a nested loop to go throght every cell then remove the ones "filled"[black] and add "empty"[white]
+    for (let y = 0; y < xySize; y++) {
+        for (let x = 0; x < xySize; x++) {
+            document.querySelector(`#coor-${x + "-" + y}`).classList.remove("filled");
+            document.querySelector(`#coor-${x + "-" + y}`).classList.add("empty");
+            cells[y][x] = 0;
+        }
+    }
+}
+
+//pure function?
+
+
+function randomize() {
+    clear(); // Clears the table
+    // Creates random filled cells
+    for (let i = 0; i < Math.floor(Math.random() * 400); i++) { // Sets the filled cells quantity randomly between 1 and 400
+
+        let x = Math.floor(Math.random() * xySize); // creates random X coordinate
+        let y = Math.floor(Math.random() * xySize); // creates random Y coordinate
+
+        cells[y][x] = 1; // Fills this cell
+    }
+    draw(); // Updates the visual grid
+}
+
+//impure function
+
+function reset(){ // Set the initial screen
+    clearTimeout(timer);
+    btnStart.innerHTML = 'Start'; // Change the text in "btnStart" button
+    btnRandom.classList.remove("clickDisable"); // Removes the "clickDisable" class on the random button to enable the button again
+    btnRestart.classList.add("clickDisable"); // Adds the "clickDisable" class on restart button to set unable
+    menu.classList.add("patternst");
+    playing = false; // Flag to know that the game stops
+    clear(); // Clears the grid
+    genNum=0; // Reset the number of generation
+    genCounter.classList.remove("avaible");
+
+    table.classList.remove("pointerDiable"); // Set the pointer events enable again
+    // Adds the instructions back
+    genCounter.innerHTML = `Please set an initial configuration before starts the game by clicking the grid, <br>
+    or choosing a pattern in the list.<br>
+    <br>
+    You can also generate a random initial configuration by clicking the "Randomize" button. <br>
+    <br>
+    The "Clear" button will wipe all the cells, no matters if the game has started or not.`;
+}
+
+let pattern1 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0],
+               [0,0,0,0,1,0,0,0,0,1,0,1,1,0,0,0,0,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],       
+               [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0],
+               [0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0],
+               [0,0,0,0,1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0],
+               [0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                ];
+let pattern2 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],       
+               [0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                ];
+let pattern3 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],       
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 ];
+let pattern4 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],       
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  ];
+
+
+                  //impure function
+
+
+function loadpat(pattern){ // Loads the pattern choosen into the grid
+    for(let i=0; i<xySize; i++){
+        for(let j=0; j<xySize;j++){
+            cells[i][j]=pattern[i][j];
+        }
+    }
+        draw();
+    }
+    
+init(); // Initialize the game
